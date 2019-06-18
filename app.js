456aea7a4a7f7c31415656249e88bc7a68f4672d
billinhit <billinhit@gmail.com>
2019-06-18 20:05:34 +0700

    - backend init

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+  , mongoose = require('mongoose')
+  , bodyParser = require('body-parser');
+
+const app = express();
+const connection = mongoose.connection;
+
+const config = require('./config');
+
+const PORT = process.env.PORT || config.app.server.port;
+const MONGO_URL = process.env['MONGODB_URI'] ? process.env['MONGODB_URI']
+  : `mongodb://${config.app.db.host}/${config.app.db.database}`;
+
+connection.on('error', (error) => {
+  console.error.bind(console, `Db connection error: ${error}`);
+});
+
+connection.once('open', () => {
+  app.listen(PORT, () => {
+    app.use(bodyParser.json());
+
+    console.log('App listening on port: ', PORT);
+  });
+});
+
+mongoose.connect(MONGO_URL, { useNewUrlParser: true });
---
+++ config/app.config.js
@@ -0,0 +1,14 @@
+/**
+ * Application config
+ * @type {{server: {port: number}, db: {connection: string}}}
+ */
+
+module.exports = {
+  server: {
+    port: 8086
+  },
+  db: {
+    host: '127.0.0.1:27017',
+    database: 'videoSearch'
+  }
+};
