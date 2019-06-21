392ca165417d656ec3c050c44157401bf401cf2d
billinhit <billinhit@gmail.com>
2019-06-21 23:59:01 +0700

    - frontend: validate latitude, longitude, radius

--- frontend/src/app/services/video.service.ts
+++ frontend/src/app/services/video.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {TokenService} from "./token.service";
 import {environment} from "../../environments/environment";
 import {Observable} from "rxjs";
 
