197219d899bc0b7d4753a0de7d9d01279f934c32
billinhit <billinhit@gmail.com>
2019-06-22 00:19:05 +0700

    - backend: - remove default keyword search

--- config/youtube.config.js
+++ config/youtube.config.js
@@ -8,7 +8,6 @@ module.exports = {
   apiKey: 'AIzaSyDwMcCfqxq5YDuWG89Y6EnRtckfbEdTAF8',
 
   part: 'snippet',
-  q: 'surfing',
   type: 'video',
   maxResults: 5,
   radiusUnit: 'km'
--- controller/video/searchVideo.controller.js
+++ controller/video/searchVideo.controller.js
@@ -17,7 +17,6 @@ module.exports = (req, res) => {
 
   let searchData = {
     part: config.youtube.part,
-    q: config.youtube.q,
     type: config.youtube.type,
     maxResults: config.youtube.maxResults,
     location: `${req.query.latitude},${req.query.longitude}`,
